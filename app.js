40930e3d43b5bb37e9a84db82f9da16edee939f9
chrishamillsnyk <88782898+chrishamillsnyk@users.noreply.github.com>
2024-05-08 20:31:27 +0100

    Update app.js

--- app.js
+++ app.js
@@ -7,3 +7,19 @@ require('./lib/startup/validateDependencies')().then(() => {
   const server = require('./server')
   server.start()
 })
+
+// validates if input email and password are correct
+function checkLogin(req, db) {
+  const sqlQuery =
+    "SELECT email FROM credentials WHERE " +
+    "email='" + req.body.email + "' AND " +
+    "password='" + req.body.password + "'";
+
+  db.query(sqlQuery, (err, result) => {
+    if (err) {
+      return false;
+    }
+
+    return result.length !== 0;
+  });
+}
